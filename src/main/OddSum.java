58cb409afa8dcb85c989524f2ad06160cc4d579f
Yacine Ouzgait <yacineouzgait59146@gmail.com>
2023-01-31 11:47:56 +0100

    Quest Exception

--- src/main/OddSum.java
+++ src/main/OddSum.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+import exception.StringException;
+
+
 public class OddSum {
 
     public static void main(String[] args) {
@@ -8,27 +11,34 @@ public class OddSum {
 
     public static int run(String[] args, boolean test) {
 
-        ArrayList<Integer> listInt = new ArrayList<>();
+        ArrayList<Integer> listInt = new ArrayList<>(); 
         int res;
-        if (!test) {
-            System.out.println("Elements' list :");
-            for (String v : args)
-                System.out.print(" " + v);
-            System.out.println();
-        }
-        for (int i = 0; i < args.length; i++) {
-            listInt.add(Integer.parseInt(args[i]));
-        }
 
-        res = sum(listInt);
-        if (test) {
-            return res;
+        try{
+            if (!test) {
+                System.out.println("Elements' list :");
+                for (String v : args)
+                    System.out.print(" " + v);
+                System.out.println();
+            }
+            for (int i = 0; i < args.length; i++) {
+                listInt.add(Integer.parseInt(args[i]));
+            }
+    
+        } catch(StringException s){
+            System.err.println("Pas de chaine de caractère svp !");
         }
+        finally {
+            res = sum(listInt);
+            if (test) {
+                return res;
+            }
+    
+            System.out.println("Sum of odd elements : " + res);
+            System.out.println("END!");
+         }
 
-        System.out.println("Sum of odd elements : " + res);
-        System.out.println("END!");
-
-        return 0;
+            return 0;
     }
 
     public static int sum(ArrayList<Integer> listInt) {
---
+++ src/main/exception/StringException.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class StringException extends RuntimeException{
+  
+    public StringException(String s) 
+    {
+       super(s);
+    }
+ 
+}
